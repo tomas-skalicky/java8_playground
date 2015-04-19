5b381bfebaadb2b77fe54e521d422654757674e1
Tomas Skalicky <skalicky.tomas@gmail.com>
2015-04-19 12:47:48 +0200

    FilesPlayground fixed

--- src/main/java/cz/skalicky/javaplayground/FilesPlayground.java
+++ src/main/java/cz/skalicky/javaplayground/FilesPlayground.java
@@ -11,11 +11,11 @@ public class FilesPlayground {
 
     public static void main(String[] args) {
 
-        final String BASE_PATH = "/home/tom/Documents/Development/Java/SourceFiles/java8/consumer/fooDir";
-        Path sPath = Paths.get(BASE_PATH + File.separator + "sourceDir" + File.separator + "testDir.txt");
+        final String BASE_PATH = "/home/tom/Documents/Development/Java/SourceFiles/java8/java8_playground/fooDir";
+        Path sPath = Paths.get(BASE_PATH + File.separator + "sourceDir" + File.separator + "testFile.txt");
         Path dPath = Paths.get(BASE_PATH + File.separator + "targetDir" + File.separator + "barDir");
         try {
-            Files.move(sPath, dPath, StandardCopyOption.ATOMIC_MOVE);
+            Files.copy(sPath, dPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ex) {
             ex.printStackTrace();
             System.err.println("Error Happened!");
