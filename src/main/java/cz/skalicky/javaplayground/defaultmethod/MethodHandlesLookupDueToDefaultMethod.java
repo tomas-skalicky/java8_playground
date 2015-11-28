c936afd27e80a5f65b7f820190a721ea3f762007
Tomas Skalicky <skalicky.tomas@gmail.com>
2015-11-28 13:25:30 +0100

    renaming

--- src/main/java/cz/skalicky/javaplayground/defaultmethod/MethodHandlesLookupDueToDefaultMethod.java
+++ src/main/java/cz/skalicky/javaplayground/defaultmethod/MethodHandlesLookupDueToDefaultMethod.java
@@ -11,7 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-public class MethodHandlesLookupTest {
+public class MethodHandlesLookupDueToDefaultMethod {
 
     public static interface MyInterface {
 
